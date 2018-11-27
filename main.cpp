8e31e75ec298d5994941b771c4767e4beef38a45
Neil Tan <neil.c.tan@gmail.com>
2018-11-27 19:09:07 +0800

    initial race condition commit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,34 @@
 #include "mbed.h"
 
-DigitalOut led(LED1);
-Thread thread;
+Thread thread0;
+Thread thread1;
 
-void led_loop() {
-    while (true) {
-        led = !led;
-        wait(0.1);
-    }
+void task0() {
+  //creating a race condition
+  while(1) {
+    int wait_time = rand() % 10;
+    wait(wait_time);
+    printf("Task 0 waited %d\r\n", wait_time);
+  }
 }
- 
+
+void task1() {
+  //creating a race condition
+  while(1) {
+    int wait_time = rand() % 10;
+    wait(wait_time);
+    printf("Task 1 waited %d\r\n", wait_time);
+  }
+}
+
 int main() {
-    thread.start(led_loop);
-    
+    number = 0;
+
+    thread0.start(task0);
+    thread1.start(task1);
+
     while (true) {
-        printf("waiting for 2 seconds\r\n");
-        wait(2);
+      //printf("print from main()\r\n");
+      wait(10);
     }
 }
