0fc2ac9c46aaab8d428f698b804bfc037120aee8
Neil Tan <neil.c.tan@gmail.com>
2018-11-27 19:42:09 +0800

    mutex example

--- main.cpp
+++ main.cpp
@@ -3,26 +3,42 @@
 Thread thread0;
 Thread thread1;
 
+int state = 0;
+Mutex my_mutex;
+
 void task0() {
   //creating a race condition
   while(1) {
-    int wait_time = rand() % 10;
-    wait(wait_time);
-    printf("Task 0 waited %d\r\n", wait_time);
+    my_mutex.lock();
+    if(state % 2 == 0) {
+      int wait_time = rand() % 10;
+      wait(wait_time);
+      printf("Task 0 waited %d\r\n", wait_time);
+      state++;
+    }
+    my_mutex.unlock();
+
+    wait(0);
   }
 }
 
 void task1() {
   //creating a race condition
   while(1) {
-    int wait_time = rand() % 10;
-    wait(wait_time);
-    printf("Task 1 waited %d\r\n", wait_time);
+    my_mutex.lock();
+    if(state % 2 == 1) {
+      int wait_time = rand() % 10;
+      wait(wait_time);
+      printf("Task 1 waited %d\r\n", wait_time);
+      state++;
+    }
+    my_mutex.unlock();
+    
+    wait(0);
   }
 }
 
 int main() {
-    number = 0;
 
     thread0.start(task0);
     thread1.start(task1);
