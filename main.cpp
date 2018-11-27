01c23a256b13ad1beb87064ffa82951bc5ba4480
Neil Tan <neil.c.tan@gmail.com>
2018-11-27 20:48:21 +0800

    added reference link

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
-
 #include "mbed.h"
 
-DigitalOut led1(LED1);
- 
-void led_loop() {
-        led1 = !led1;
-        wait(0.1);
+//https://os.mbed.com/blog/entry/Simplify-your-code-with-mbed-events/
+
+DigitalOut led(LED1);
+InterruptIn btn(SW2);
+
+EventQueue queue;
+
+void do_something_outside_irq() {
+  // this does not run in the ISR
+  printf("Toggle LED!\r\n");
 }
- 
+
+void do_something_in_irq() {
+  // this runs in the ISR
+  led = !led;
+
+  // then defer the printf call to the other thread
+  queue.call(&do_something_outside_irq);
+}
+
 int main() {
-    while (true) {
-        led_loop();
-        printf("waiting for 2 seconds\r\n");
-        wait(2);
-    }
+  Thread eventThread;
+  eventThread.start(callback(&queue, &EventQueue::dispatch_forever));
+
+  btn.fall(&do_something_in_irq);
+
+  wait(osWaitForever);
 }
